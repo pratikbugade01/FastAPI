03381e7f372fa8b68f2467fc9c9c663c4c8ccb1c
Pratik Bugade <pratikbugade555@gmail.com>
2025-11-30 19:51:10 +0530

    added path function

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def view():
     return data
 
 @app.get("/patient/{patient_id}")
-def view_patient(patient_id: str):
+def view_patient(patient_id: str = Path(..., description="The ID of the patient in the DB",example="P001")):
 
     data = load_data()
 
